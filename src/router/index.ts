9d860cacb11bb64c285846474ee7dd69610d1c5b
edoriggio <edo.riggio19@gmail.com>
2025-09-24 13:49:21 +0200

    changed to webhashhistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import HomePage from '@/pages/HomePage.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: "/", component: HomePage }
   ],
